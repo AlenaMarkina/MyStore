4855a679517e24c21dc0008588a106b354cc90a1
Alena Markina <markina.a7@yandex.ru>
2023-06-23 18:33:00 +0300

    django project in progress

--- login/views.py
+++ login/views.py
@@ -1,7 +1,12 @@
 from django.views import View
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 class Login(View):
     def get(self, request):
         return render(request, 'login/index.html')
+
+    def post(self, request):
+        return JsonResponse(request.POST, json_dumps_params={'indent': 4})
+
--- store/views.py
+++ store/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class ProductSingleView(View):
+    def get(self, request):
+        return render(request, 'store/product-single.html')
+
+
+class CartView(View):
+    def get(self, requests):
+        return render(requests, 'store/cart.html')
+
+
+class ShopView(View):
+    def get(self, requests):
+        context = {'data': [{'name': 'Bell Pepper',
+                             'discount': 30,
+                             'price_before': 120.00,
+                             'price_after': 80.00,
+                             'url': 'store/images/product-1.jpg'},
+                            {'name': 'Strawberry',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-2.jpg'},
+                            {'name': 'Green Beans',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-3.jpg'},
+                            {'name': 'Purple Cabbage',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-4.jpg'},
+                            {'name': 'Tomatoe',
+                             'discount': 30,
+                             'price_before': 120.00,
+                             'price_after': 80.00,
+                             'url': 'store/images/product-5.jpg'},
+                            {'name': 'Brocolli',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-6.jpg'},
+                            {'name': 'Carrots',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-7.jpg'},
+                            {'name': 'Fruit Juice',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-8.jpg'},
+                            {'name': 'Onion',
+                             'discount': 30,
+                             'price_before': 120.00,
+                             'price_after': 80.00,
+                             'url': 'store/images/product-9.jpg'},
+                            {'name': 'Apple',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-10.jpg'},
+                            {'name': 'Garlic',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-11.jpg'},
+                            {'name': 'Chilli',
+                             'discount': None,
+                             'price_before': 120.00,
+                             'url': 'store/images/product-12.jpg'}
+                            ]
+                   }
+        return render(requests, 'store/shop.html', context)
