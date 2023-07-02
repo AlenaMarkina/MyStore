69d8cdd72d0a1d2560768a2eae82858e7268393a
Alena Markina <markina.a7@yandex.ru>
2023-07-02 22:29:02 +0300

    Home work 2 in progress

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import Login
+from .views import LoginView
 
+
+app_name = 'login'
 
 urlpatterns = [
-    path('', Login.as_view())
+    path('', LoginView.as_view(), name='login')
 ]
