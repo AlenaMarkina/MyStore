51a42c89c50bd23e86f1cc4e32567d40f381123d
Alena Markina <markina.a7@yandex.ru>
2023-07-07 21:19:16 +0300

    Home work 2 in progress

--- login/views.py
+++ login/views.py
@@ -2,7 +2,9 @@ from django.views import View
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
 
 
 class LoginView(View):
@@ -20,3 +22,26 @@ class LoginView(View):
                 return redirect('store:shop')
         return request('login:login')
 
+
+class LogoutView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            logout(request)
+        return redirect('store:shop')
+
+
+class CreateAccountView(View):
+    def get(self, request):
+        return render(request, "login/create_account.html")
+
+    def post(self, request):
+        form = CustomUserCreationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            login(request, user)
+            return redirect('store:shop')
+        return render(request, 'login/create_account.html', context={'errors': form.errors})
