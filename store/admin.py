9b59a216b8126bcc4c4fbbe55ee28b9aa3d66c55
Маркина Алена Андреевна <a.markina@PARUS.LOCAL>
2023-07-07 15:50:03 +0300

    Exam in progress

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category, Product, Discount
+from .models import Category, Product, Discount, Wishlist
 
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Discount)
+admin.site.register(Wishlist)
